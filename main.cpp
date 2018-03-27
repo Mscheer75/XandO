5bc90f05481e988fca8890424e90dadd608dd9f8
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-03-27 12:11:08 -0700

    gahhhhhh

--- main.cpp
+++ main.cpp
@@ -65,7 +65,7 @@ int main()
     {
         for (int j=0; j <3; j++)
         {
-            cout << board[j][i] << " "; 
+            cout << board[i][j] << " "; 
             if ( j == 2)
             {
                 cout << endl;
